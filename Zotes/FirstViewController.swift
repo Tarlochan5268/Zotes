7ada730bf1d280867da0194ba7eb942a0e7e6024
Kishore Narang <narangkishore98@gmail.com>
2019-10-30 15:38:19 -0400

    Initial Commit

---
+++ Zotes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Zotes
+//
+//  Created by Kishore Narang on 2019-10-30.
+//  Copyright © 2019 Zero. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
